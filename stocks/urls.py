9e7a95017a98f8b5c8f15d0846191c472545b91c
kristina <christinavader@gmail.com>
2020-12-24 21:08:25 +0300

    Templates added: Home, Study, Analyze, Explore.

--- stocks/urls.py
+++ stocks/urls.py
@@ -2,5 +2,10 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.ticker_list, name='ticker_list'),
+    path('', views.home_page, name='home_page'),
+    path('home/', views.home_page, name='home_page'),
+    path('home/tickers/', views.home_tickers, name='home_tickers'),
+    path('study/', views.study_page, name='study_page'),
+    path('analyze/', views.analyze_page, name='analyze_page'),
+    path('explore/', views.explore_page, name='explore_page'),
 ]
--- stocks/views.py
+++ stocks/views.py
@@ -2,6 +2,23 @@ from django.shortcuts import render
 from django.utils import timezone
 from .models import Ticker
 
-def ticker_list(request):
+
+def home_page(request):
+    return render(request, 'stocks/home.html', {})
+
+
+def home_tickers(request):
     tickers = Ticker.objects.order_by('name')
-    return render(request, 'stocks/ticker_list.html', {'tickers': tickers})
+    return render(request, 'stocks/home_tickers.html', {'tickers': tickers})
+
+
+def study_page(request):
+    return render(request, 'stocks/study.html', {})
+
+
+def analyze_page(request):
+    return render(request, 'stocks/analyze.html', {})
+
+
+def explore_page(request):
+    return render(request, 'stocks/explore.html', {})
